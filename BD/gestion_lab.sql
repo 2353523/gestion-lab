c5e9a4282e08d0dc29f42f03248eb46942554fd4
Seydina <23535@isme.esp.mr>
2025-03-01 01:50:13 +0000

    sql changé

--- BD/gestion_lab.sql
+++ BD/gestion_lab.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 28, 2025 at 01:12 PM
+-- Generation Time: Mar 01, 2025 at 02:46 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -29,15 +29,12 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `article` (
   `id_article` int(11) NOT NULL AUTO_INCREMENT,
   `nom_article` varchar(150) NOT NULL,
-  `id_type` int(11) NOT NULL,
   `unite_mesure` varchar(50) NOT NULL,
-  `id_recu` int(11) NOT NULL,
   `id_stock_magasin` int(11) NOT NULL,
-  `id_type_1` int(11) NOT NULL,
+  `id_type` int(11) NOT NULL,
   PRIMARY KEY (`id_article`),
-  KEY `id_recu` (`id_recu`),
   KEY `id_stock_magasin` (`id_stock_magasin`),
-  KEY `id_type_1` (`id_type_1`)
+  KEY `id_type` (`id_type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -62,10 +59,10 @@ CREATE TABLE IF NOT EXISTS `historique_` (
   `id_historique` int(11) NOT NULL AUTO_INCREMENT,
   `id_tp` int(11) NOT NULL,
   `id_prof` int(11) NOT NULL,
-  `degradation` varchar(50) NOT NULL,
+  `degradation` tinyint(1) NOT NULL DEFAULT '0',
   `id_article` int(11) NOT NULL,
-  `quantite` int(11) NOT NULL,
-  `date_utilisation` datetime NOT NULL,
+  `quantite` int(11) NOT NULL DEFAULT '0',
+  `date_utilisation` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `commentaire` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_historique`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
@@ -81,6 +78,20 @@ CREATE TABLE IF NOT EXISTS `laboratoire` (
   `nom_laboratoire` varchar(50) NOT NULL,
   PRIMARY KEY (`id_laboratoire`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ligne_recu`
+--
+
+CREATE TABLE IF NOT EXISTS `ligne_recu` (
+  `id_article` int(11) NOT NULL DEFAULT '0',
+  `id_recu` int(11) NOT NULL DEFAULT '0',
+  `quantite` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_article`,`id_recu`),
+  KEY `id_recu` (`id_recu`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -105,7 +116,7 @@ CREATE TABLE IF NOT EXISTS `professeur` (
   `prenom` varchar(50) NOT NULL,
   `nom` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `telephone` varchar(8) NOT NULL,
+  `telephone` varchar(15) NOT NULL,
   PRIMARY KEY (`id_prof`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `telephone` (`telephone`)
@@ -119,18 +130,14 @@ CREATE TABLE IF NOT EXISTS `professeur` (
 
 CREATE TABLE IF NOT EXISTS `recu` (
   `id_recu` int(11) NOT NULL AUTO_INCREMENT,
-  `id_tp` int(11) NOT NULL,
-  `id_article_1` int(11) NOT NULL,
-  `id_prof` int(11) NOT NULL,
-  `date_emission` datetime NOT NULL,
-  `degradation` varchar(50) NOT NULL DEFAULT '0',
+  `date_emission` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `degradation` tinyint(1) NOT NULL DEFAULT '0',
   `observations` varchar(255) DEFAULT NULL,
-  `quantite` int(11) NOT NULL,
-  `id_historique` int(11) NOT NULL,
-  `id_prof_1` int(11) NOT NULL,
+  `id_tp` int(11) NOT NULL,
+  `id_prof` int(11) NOT NULL,
   PRIMARY KEY (`id_recu`),
-  KEY `id_historique` (`id_historique`),
-  KEY `id_prof_1` (`id_prof_1`)
+  KEY `id_tp` (`id_tp`),
+  KEY `id_prof` (`id_prof`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -140,14 +147,11 @@ CREATE TABLE IF NOT EXISTS `recu` (
 --
 
 CREATE TABLE IF NOT EXISTS `stock_laboratoire` (
-  `id_article_1` int(11) NOT NULL DEFAULT '0',
-  `id_laboratoire_1` int(11) NOT NULL DEFAULT '0',
-  `id_stock_labo` int(11) NOT NULL,
-  `id_article` int(11) NOT NULL,
+  `id_article` int(11) NOT NULL DEFAULT '0',
+  `id_laboratoire` int(11) NOT NULL DEFAULT '0',
   `quantite` int(11) NOT NULL DEFAULT '0',
-  `id_laboratoire` int(11) NOT NULL,
-  PRIMARY KEY (`id_article_1`,`id_laboratoire_1`),
-  KEY `id_laboratoire_1` (`id_laboratoire_1`)
+  PRIMARY KEY (`id_article`,`id_laboratoire`),
+  KEY `id_laboratoire` (`id_laboratoire`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -158,7 +162,6 @@ CREATE TABLE IF NOT EXISTS `stock_laboratoire` (
 
 CREATE TABLE IF NOT EXISTS `stock_magasin` (
   `id_stock_magasin` int(11) NOT NULL AUTO_INCREMENT,
-  `id_article` int(11) NOT NULL,
   `quantite` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_stock_magasin`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
@@ -174,17 +177,14 @@ CREATE TABLE IF NOT EXISTS `tp` (
   `nom_tp` varchar(100) NOT NULL,
   `heure_debut` datetime NOT NULL,
   `heure_fin` datetime NOT NULL,
-  `id_prof` int(11) NOT NULL,
-  `id_matiere` int(11) NOT NULL,
-  `anne_scolaire` varchar(9) NOT NULL,
+  `annee_scolaire` varchar(9) NOT NULL,
   `id_laboratoire` int(11) NOT NULL,
-  `id_laboratoire_1` int(11) NOT NULL,
-  `id_matiere_1` int(11) NOT NULL,
-  `id_prof_1` int(11) NOT NULL,
+  `id_matiere` int(11) NOT NULL,
+  `id_prof` int(11) NOT NULL,
   PRIMARY KEY (`id_tp`),
-  KEY `id_laboratoire_1` (`id_laboratoire_1`),
-  KEY `id_matiere_1` (`id_matiere_1`),
-  KEY `id_prof_1` (`id_prof_1`)
+  KEY `id_laboratoire` (`id_laboratoire`),
+  KEY `id_matiere` (`id_matiere`),
+  KEY `id_prof` (`id_prof`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -197,9 +197,8 @@ CREATE TABLE IF NOT EXISTS `type` (
   `id_type` int(11) NOT NULL AUTO_INCREMENT,
   `nom_type` varchar(50) NOT NULL,
   `id_categorie` int(11) NOT NULL,
-  `id_categorie_1` int(11) NOT NULL,
   PRIMARY KEY (`id_type`),
-  KEY `id_categorie_1` (`id_categorie_1`)
+  KEY `id_categorie` (`id_categorie`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
@@ -210,37 +209,43 @@ CREATE TABLE IF NOT EXISTS `type` (
 -- Constraints for table `article`
 --
 ALTER TABLE `article`
-  ADD CONSTRAINT `article_ibfk_1` FOREIGN KEY (`id_recu`) REFERENCES `recu` (`id_recu`),
-  ADD CONSTRAINT `article_ibfk_2` FOREIGN KEY (`id_stock_magasin`) REFERENCES `stock_magasin` (`id_stock_magasin`),
-  ADD CONSTRAINT `article_ibfk_3` FOREIGN KEY (`id_type_1`) REFERENCES `type` (`id_type`);
+  ADD CONSTRAINT `article_ibfk_1` FOREIGN KEY (`id_stock_magasin`) REFERENCES `stock_magasin` (`id_stock_magasin`),
+  ADD CONSTRAINT `article_ibfk_2` FOREIGN KEY (`id_type`) REFERENCES `type` (`id_type`);
+
+--
+-- Constraints for table `ligne_recu`
+--
+ALTER TABLE `ligne_recu`
+  ADD CONSTRAINT `ligne_recu_ibfk_1` FOREIGN KEY (`id_article`) REFERENCES `article` (`id_article`) ON DELETE CASCADE,
+  ADD CONSTRAINT `ligne_recu_ibfk_2` FOREIGN KEY (`id_recu`) REFERENCES `recu` (`id_recu`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `recu`
 --
 ALTER TABLE `recu`
-  ADD CONSTRAINT `recu_ibfk_1` FOREIGN KEY (`id_historique`) REFERENCES `historique_` (`id_historique`),
-  ADD CONSTRAINT `recu_ibfk_2` FOREIGN KEY (`id_prof_1`) REFERENCES `professeur` (`id_prof`);
+  ADD CONSTRAINT `recu_ibfk_1` FOREIGN KEY (`id_tp`) REFERENCES `tp` (`id_tp`),
+  ADD CONSTRAINT `recu_ibfk_2` FOREIGN KEY (`id_prof`) REFERENCES `professeur` (`id_prof`);
 
 --
 -- Constraints for table `stock_laboratoire`
 --
 ALTER TABLE `stock_laboratoire`
-  ADD CONSTRAINT `stock_laboratoire_ibfk_1` FOREIGN KEY (`id_article_1`) REFERENCES `article` (`id_article`),
-  ADD CONSTRAINT `stock_laboratoire_ibfk_2` FOREIGN KEY (`id_laboratoire_1`) REFERENCES `laboratoire` (`id_laboratoire`);
+  ADD CONSTRAINT `stock_laboratoire_ibfk_1` FOREIGN KEY (`id_article`) REFERENCES `article` (`id_article`) ON DELETE CASCADE,
+  ADD CONSTRAINT `stock_laboratoire_ibfk_2` FOREIGN KEY (`id_laboratoire`) REFERENCES `laboratoire` (`id_laboratoire`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `tp`
 --
 ALTER TABLE `tp`
-  ADD CONSTRAINT `tp_ibfk_1` FOREIGN KEY (`id_laboratoire_1`) REFERENCES `laboratoire` (`id_laboratoire`),
-  ADD CONSTRAINT `tp_ibfk_2` FOREIGN KEY (`id_matiere_1`) REFERENCES `matiere` (`id_matiere`),
-  ADD CONSTRAINT `tp_ibfk_3` FOREIGN KEY (`id_prof_1`) REFERENCES `professeur` (`id_prof`);
+  ADD CONSTRAINT `tp_ibfk_1` FOREIGN KEY (`id_laboratoire`) REFERENCES `laboratoire` (`id_laboratoire`),
+  ADD CONSTRAINT `tp_ibfk_2` FOREIGN KEY (`id_matiere`) REFERENCES `matiere` (`id_matiere`),
+  ADD CONSTRAINT `tp_ibfk_3` FOREIGN KEY (`id_prof`) REFERENCES `professeur` (`id_prof`);
 
 --
 -- Constraints for table `type`
 --
 ALTER TABLE `type`
-  ADD CONSTRAINT `type_ibfk_1` FOREIGN KEY (`id_categorie_1`) REFERENCES `categorie` (`id_categorie`);
+  ADD CONSTRAINT `type_ibfk_1` FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id_categorie`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
