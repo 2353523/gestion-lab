88ca93b50cb74c115b6a5d7eea7b0704c002c769
sidahmed43 <sidahmedmeden07@gmail.com>
2025-04-11 17:30:52 +0000

    sidahmed43

--- BD/gestion_lab (14).sql
+++ BD/gestion_lab (14).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 04, 2025 at 03:22 PM
+-- Generation Time: Apr 11, 2025 at 05:30 PM
 -- Server version: 9.1.0
 -- PHP Version: 8.3.14
 
@@ -34,24 +34,32 @@ CREATE TABLE IF NOT EXISTS `article` (
   `unite_mesure` varchar(50) NOT NULL,
   `date_expiration` datetime DEFAULT NULL,
   `id_type` int NOT NULL,
+  `ghs_codes` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_article`),
   KEY `id_type` (`id_type`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `article`
 --
 
-INSERT INTO `article` (`id_article`, `nom_article`, `unite_mesure`, `date_expiration`, `id_type`) VALUES
-(1, 'becher', 'litre', NULL, 1),
-(2, 'meden', 'litre', NULL, 1),
-(7, 'pipette 20mL', 'unité', '2025-04-17 00:00:00', 1),
-(8, 'bêcher  50mL', 'unité', NULL, 4),
-(9, 'pippet10ml', 'unité', NULL, 1),
-(10, 'NaOH', 'litre', NULL, 3),
-(11, 'NaOH', 'litre', NULL, 3),
-(12, 'HCl ', 'litre', NULL, 2),
-(13, 'clim 12', 'unité', '2026-08-13 00:00:00', 5);
+INSERT INTO `article` (`id_article`, `nom_article`, `unite_mesure`, `date_expiration`, `id_type`, `ghs_codes`) VALUES
+(1, 'becher', 'litre', NULL, 1, NULL),
+(2, 'meden', 'litre', NULL, 1, NULL),
+(7, 'pipette 20mL', 'unité', '2025-04-17 00:00:00', 1, NULL),
+(8, 'bêcher  50mL', 'unité', NULL, 4, NULL),
+(9, 'pippet10ml', 'unité', NULL, 1, NULL),
+(10, 'NaOH', 'litre', NULL, 3, '06'),
+(11, 'NaOH', 'litre', NULL, 3, '06'),
+(12, 'HCl 2mol/L', 'litre', NULL, 2, ''),
+(13, 'clim 12', 'unité', NULL, 5, NULL),
+(14, 'sidahmed', 'litre', '2025-04-18 00:00:00', 2, NULL),
+(15, 'sidahmed', 'kg', '2025-04-11 00:00:00', 1, NULL),
+(16, 'sidahmed', 'kg', '2025-04-11 00:00:00', 1, '01,02'),
+(18, 'sidahmed43', 'unité', '2025-05-07 00:00:00', 2, '01'),
+(20, 'dedwwww', 'unité', '2029-04-04 00:00:00', 2, '05'),
+(21, '23', 'unité', NULL, 2, '01'),
+(22, '43', 'paquet', '2029-04-04 00:00:00', 2, '06,09');
 
 -- --------------------------------------------------------
 
@@ -87,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `laboratoire` (
   `nom_laboratoire` varchar(50) NOT NULL,
   `capacite` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_laboratoire`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `laboratoire`
@@ -128,9 +136,11 @@ INSERT INTO `ligne_recu` (`id_article`, `id_recu`, `quantite`, `degradation_quan
 (8, 71, 1, 0),
 (8, 77, 1, 0),
 (8, 79, 8, 1),
+(8, 84, 1, 0),
 (9, 78, 1, 1),
 (9, 79, 1, 0),
 (9, 80, 7, 1),
+(9, 82, 2, 0),
 (10, 80, 9, 2),
 (10, 81, 7, 1),
 (12, 69, 1, 0),
@@ -139,7 +149,8 @@ INSERT INTO `ligne_recu` (`id_article`, `id_recu`, `quantite`, `degradation_quan
 (12, 75, 5, 2),
 (12, 76, 2, 2),
 (13, 79, 2, 1),
-(13, 80, 2, 1);
+(13, 80, 2, 1),
+(22, 85, 3, 1);
 
 -- --------------------------------------------------------
 
@@ -153,7 +164,7 @@ CREATE TABLE IF NOT EXISTS `matiere` (
   `nom_matiere` varchar(100) NOT NULL,
   `niveau` enum('L1','L2','L3') NOT NULL,
   PRIMARY KEY (`id_matiere`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `matiere`
@@ -183,7 +194,7 @@ CREATE TABLE IF NOT EXISTS `professeur` (
   PRIMARY KEY (`id_prof`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `telephone` (`telephone`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `professeur`
@@ -191,11 +202,15 @@ CREATE TABLE IF NOT EXISTS `professeur` (
 
 INSERT INTO `professeur` (`id_prof`, `prenom`, `nom`, `email`, `telephone`) VALUES
 (3, 'yeslem', 'teghra', '23543@isme.esp.mr', '34303065'),
-(4, 'a', 'a', '07@gmail.com', '00000000'),
+(4, 'asiiiiiiiii', 'a', '07@gmail.com', '00000000'),
 (6, 'med', 'sidi', '23543@isme.esp.mre', '34303464'),
 (7, 'Dr.ahmed', 'ahmed', 'sidahmedmeden7@gmail.com', '34303445'),
 (8, 'sidahmed', 'meden', 'sidahmedmeden@gmail.com', '65323455'),
-(9, 'bebouh', 'diagana', 'the9rde@gmail.com', '34304465');
+(9, 'bebouh', 'diagana', 'the9rde@gmail.com', '34304465'),
+(12, 'a', 'a', '', ''),
+(19, 'yghu', '87788', 'sidahmedmei8oden07@gmail.com', '44444444'),
+(20, 'hhj', 'uyy', 'sidqwwi23543@isme.esp.mr', '23332222'),
+(21, 'dff', 'dffd', 'sidahmedme333den07@gmail.com', '11111112');
 
 -- --------------------------------------------------------
 
@@ -215,7 +230,7 @@ CREATE TABLE IF NOT EXISTS `recu` (
   UNIQUE KEY `unique_tp` (`id_tp`),
   KEY `id_tp` (`id_tp`),
   KEY `id_prof` (`id_prof`)
-) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=86 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `recu`
@@ -235,7 +250,10 @@ INSERT INTO `recu` (`id_recu`, `date_emission`, `degradation`, `observations`, `
 (78, '2025-04-03 01:12:44', 0, 'bien passe', 164, 8),
 (79, '2025-04-03 02:15:55', 0, 'siiiiiiiiiiii\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\nd\r\n\r\n\r\nd\r\n\r\n\r\n\r\nd', 161, 8),
 (80, '2025-04-03 03:48:19', 0, 'siiiiiiiiii', 160, 8),
-(81, '2025-04-03 04:51:26', 0, 'siii tooo', 162, 8);
+(81, '2025-04-03 04:51:26', 0, 'siii tooo', 162, 8),
+(82, '2025-04-05 04:18:52', 0, '', 168, 8),
+(84, '2025-04-09 16:40:15', 0, '', 199, 3),
+(85, '2025-04-11 17:22:24', 0, '', 204, 8);
 
 -- --------------------------------------------------------
 
@@ -275,7 +293,9 @@ INSERT INTO `stock_laboratoire` (`id_lot`, `id_laboratoire`, `quantite`) VALUES
 (10, 5, 1),
 (10, 7, 5),
 (11, 2, 2),
-(11, 6, 0);
+(11, 6, 0),
+(16, 1, 1),
+(17, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -292,7 +312,7 @@ CREATE TABLE IF NOT EXISTS `stock_magasin` (
   `date_ajout` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_lot`),
   KEY `id_article` (`id_article`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `stock_magasin`
@@ -300,12 +320,16 @@ CREATE TABLE IF NOT EXISTS `stock_magasin` (
 
 INSERT INTO `stock_magasin` (`id_lot`, `id_article`, `quantite`, `date_expiration`, `date_ajout`) VALUES
 (5, 7, 20, '2025-04-17 00:00:00', '2025-03-24 22:31:28'),
-(6, 8, 2, NULL, '2025-03-24 22:31:52'),
-(7, 9, 5, NULL, '2025-03-25 01:19:39'),
-(8, 10, 0, NULL, '2025-03-25 01:28:23'),
+(6, 8, 12, NULL, '2025-03-24 22:31:52'),
+(7, 9, 15, NULL, '2025-03-25 01:19:39'),
+(8, 10, 15, NULL, '2025-03-25 01:28:23'),
 (9, 11, 20, NULL, '2025-03-25 01:43:06'),
-(10, 12, 2, NULL, '2025-03-25 07:38:16'),
-(11, 13, 4, '2026-08-13 00:00:00', '2025-03-26 00:33:49');
+(10, 12, 20, NULL, '2025-03-25 07:38:16'),
+(11, 13, 14, NULL, '2025-03-26 00:33:49'),
+(12, 14, 2, '2025-04-18 00:00:00', '2025-04-11 00:51:07'),
+(13, 15, 5, '2025-04-11 00:00:00', '2025-04-11 14:20:19'),
+(16, 21, 2, NULL, '2025-04-11 15:41:51'),
+(17, 22, 43, '2029-04-04 00:00:00', '2025-04-11 15:46:58');
 
 -- --------------------------------------------------------
 
@@ -328,7 +352,7 @@ CREATE TABLE IF NOT EXISTS `tp` (
   KEY `id_laboratoire` (`id_laboratoire`),
   KEY `id_matiere` (`id_matiere`),
   KEY `id_prof` (`id_prof`)
-) ENGINE=InnoDB AUTO_INCREMENT=168 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=206 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tp`
@@ -396,7 +420,6 @@ INSERT INTO `tp` (`id_tp`, `nom_tp`, `heure_debut`, `heure_fin`, `annee_scolaire
 (149, 'aaa', '2025-04-02 09:45:00', '2025-04-02 11:15:00', '2024-2025', 7, 6, 4, 0),
 (150, 'aaa', '2025-04-02 11:30:00', '2025-04-02 13:00:00', '2024-2025', 7, 6, 4, 1),
 (151, 'aaa', '2025-04-02 15:10:00', '2025-04-02 16:40:00', '2024-2025', 7, 6, 4, 0),
-(153, 'acier', '2025-04-02 08:00:00', '2025-04-02 09:30:00', '2024-2025', 8, 4, 9, 0),
 (154, 'acier', '2025-04-02 09:45:00', '2025-04-02 11:15:00', '2024-2025', 8, 4, 9, 0),
 (155, 'acier', '2025-04-02 11:30:00', '2025-04-02 13:00:00', '2024-2025', 8, 4, 9, 0),
 (156, 'acier', '2025-04-02 15:10:00', '2025-04-02 16:40:00', '2024-2025', 8, 4, 9, 0),
@@ -409,7 +432,40 @@ INSERT INTO `tp` (`id_tp`, `nom_tp`, `heure_debut`, `heure_fin`, `annee_scolaire
 (164, 'cinetique1', '2025-04-03 08:00:00', '2025-04-03 09:30:00', '2024-2025', 6, 6, 8, 1),
 (165, 'ss', '2025-04-04 08:00:00', '2025-04-04 09:30:00', '2024-2025', 2, 1, 3, 0),
 (166, 'ss', '2025-04-04 09:45:00', '2025-04-04 11:15:00', '2024-2025', 2, 1, 3, 0),
-(167, 'ss', '2025-04-04 11:30:00', '2025-04-04 13:00:00', '2024-2025', 2, 1, 3, 0);
+(167, 'ss', '2025-04-04 11:30:00', '2025-04-04 13:00:00', '2024-2025', 2, 1, 3, 0),
+(168, 'cinetique', '2025-04-05 08:00:00', '2025-04-05 09:30:00', '2024-2025', 6, 6, 8, 1),
+(169, 'cinetique', '2025-04-05 09:45:00', '2025-04-05 11:15:00', '2024-2025', 6, 6, 8, 0),
+(170, 'cinetique', '2025-04-05 11:30:00', '2025-04-05 13:00:00', '2024-2025', 6, 6, 8, 0),
+(171, 'cinetique', '2025-04-05 15:10:00', '2025-04-05 16:40:00', '2024-2025', 6, 6, 8, 0),
+(172, 'cinetique', '2025-04-05 17:00:00', '2025-04-05 18:30:00', '2024-2025', 6, 6, 8, 0),
+(173, 'cinetique', '2025-04-05 08:00:00', '2025-04-05 09:30:00', '2024-2025', 1, 2, 3, 0),
+(174, 'cinetique', '2025-04-05 09:45:00', '2025-04-05 11:15:00', '2024-2025', 1, 2, 3, 0),
+(175, 'cinetique', '2025-04-05 11:30:00', '2025-04-05 13:00:00', '2024-2025', 1, 2, 3, 0),
+(176, 'cinetique', '2025-04-05 15:10:00', '2025-04-05 16:40:00', '2024-2025', 1, 2, 3, 0),
+(177, 'cinetique', '2025-04-05 17:00:00', '2025-04-05 18:30:00', '2024-2025', 1, 2, 3, 0),
+(178, 'tpppp', '2025-04-05 08:00:00', '2025-04-05 09:30:00', '2024-2025', 7, 5, 9, 0),
+(179, 'tpppp', '2025-04-05 09:45:00', '2025-04-05 11:15:00', '2024-2025', 7, 5, 9, 0),
+(180, 'tpppp', '2025-04-05 11:30:00', '2025-04-05 13:00:00', '2024-2025', 7, 5, 9, 0),
+(181, 'tpppp', '2025-04-05 15:10:00', '2025-04-05 16:40:00', '2024-2025', 7, 5, 9, 0),
+(182, 'tpppp', '2025-04-05 17:00:00', '2025-04-05 18:30:00', '2024-2025', 7, 5, 9, 0),
+(183, 'cinetique', '2025-04-06 08:00:00', '2025-04-06 09:30:00', '2024-2025', 6, 1, 8, 0),
+(184, 'cinetique', '2025-04-06 09:45:00', '2025-04-06 11:15:00', '2024-2025', 6, 1, 8, 0),
+(185, 'acier1111111', '2025-04-02 08:00:00', '2025-04-02 09:30:00', '2024-2025', 8, 4, 9, 0),
+(189, 'thermodynamic', '2025-04-07 15:10:00', '2025-04-07 16:40:00', '2024-2025', 1, 6, 7, 0),
+(190, 'thermodynamic', '2025-04-07 17:00:00', '2025-04-07 18:30:00', '2024-2025', 1, 6, 7, 0),
+(192, 'thermodynamics', '2025-04-07 08:00:00', '2025-04-07 09:30:00', '2024-2025', 1, 6, 7, 0),
+(193, 'thermodynamics', '2025-04-07 11:30:00', '2025-04-07 13:00:00', '2024-2025', 1, 6, 7, 1),
+(195, 'cinetique', '2025-04-08 09:45:00', '2025-04-08 11:15:00', '2024-2025', 1, 2, 7, 0),
+(196, 'cinetique', '2025-04-08 11:30:00', '2025-04-08 13:00:00', '2024-2025', 1, 2, 7, 0),
+(197, 'cinetique', '2025-04-08 15:10:00', '2025-04-08 16:40:00', '2024-2025', 1, 2, 7, 0),
+(198, 'cinetique', '2025-04-08 17:00:00', '2025-04-08 18:30:00', '2024-2025', 1, 2, 7, 0),
+(199, 'cinetique', '2025-04-09 08:00:00', '2025-04-09 09:30:00', '2024-2025', 1, 1, 3, 1),
+(200, 'cinetique', '2025-04-09 09:45:00', '2025-04-09 11:15:00', '2024-2025', 1, 1, 3, 0),
+(201, 'cinetique', '2025-04-09 11:30:00', '2025-04-09 13:00:00', '2024-2025', 1, 1, 3, 0),
+(202, 'cinetique', '2025-04-09 15:10:00', '2025-04-09 16:40:00', '2024-2025', 1, 1, 3, 0),
+(203, 'cinetique', '2025-04-09 17:00:00', '2025-04-09 18:30:00', '2024-2025', 1, 1, 3, 0),
+(204, 'cinetique', '2025-04-11 08:00:00', '2025-04-11 09:30:00', '2024-2025', 1, 2, 8, 1),
+(205, 'cinetique', '2025-04-11 09:45:00', '2025-04-11 11:15:00', '2024-2025', 1, 2, 8, 0);
 
 -- --------------------------------------------------------
 
@@ -446,20 +502,26 @@ INSERT INTO `type` (`id_type`, `nom_type`, `id_categorie`) VALUES
 DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` enum('admin','user') NOT NULL DEFAULT 'user',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `gmail` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`id`, `username`, `password`, `role`) VALUES
-(5, 'admin', 'scrypt:32768:8:1$clT7v7hQyqaPkQ83$0743f96b5698e29ffc2676ee08cedbd34c8cc0bf8db7efa837e9b25cf83f6571328253133930b672ab32544846bc62f95ee432a474ac602ee8764f866210d0c3', 'admin'),
-(6, 'user', 'scrypt:32768:8:1$8eOYiv0OZoQkVAjs$a162834f8313d9950b5547da4961549423e968a4b03d7b03d9c6fb815fc07efc717c8019b18e6c0a28dda15f865f97219090906d555894b94fe4153b045d3e61', 'user');
+INSERT INTO `utilisateur` (`id`, `username`, `email`, `password`, `role`) VALUES
+(5, 'admin', 'admin@example.com', 'scrypt:32768:8:1$PkITUqxRGMnlOiqO$d3326cbceb722ac77f6e3831bb27503503e604145406d1e6c12947359c4f9648395421bbb471459314320f9f449ad8bdaee3a9699fec7ed91bb1d5af50998add', 'admin'),
+(6, 'user', 'user@example.com', 'scrypt:32768:8:1$8eOYiv0OZoQkVAjs$a162834f8313d9950b5547da4961549423e968a4b03d7b03d9c6fb815fc07efc717c8019b18e6c0a28dda15f865f97219090906d555894b94fe4153b045d3e61', 'user'),
+(15, 'sidahmed2', '23543@isme.esp.mr', 'scrypt:32768:8:1$BYaOrLrN5WNpJC3Z$145644f14a585c88bdfa4e748b1f19f17d51643c1b4db2bc59d6841169c6bad7133250dc7f05b454f1345deb1d6def42fd58bbd51e77982926f72f2160ab3b58', 'admin'),
+(16, 'ddw', 'sidahmedmeden07@gmail.com', 'scrypt:32768:8:1$0mt6eLp4B4o0KiJB$01bd08c8202fce4eb1096c59da28290ffc78ecd782c648986608e55f4d2444614be354c25d20712da4c552facccfdc0f9fa405e1ea82cf0d153f504740afbb0f', 'admin'),
+(17, 'ddw42', '23542@isme.esp.mr', 'scrypt:32768:8:1$iBGKPbN71ggQDCfx$0fe34f6d15ea6830fe326ca505f345a6cbe3286c26f9099c465ab7575f430f0581f821d6cfdfb8321341708e058063d96e29ba1def6283cc9a8f707a577e0e24', 'admin');
 
 --
 -- Constraints for dumped tables
